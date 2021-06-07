c051b54167d65f13ff5c7e52d5cc4d8f374831c8
Matt <matheus.canassa@hotmail.com>
2021-06-07 17:16:25 -0300

    feat: initial variable, back sensors and init-function sets
    we are trying to build a proper initial setting for many strategies and comebacks

--- 00-NeonTestes.py
+++ 00-NeonTestes.py
@@ -1,54 +1,69 @@
-flag = 0 # 0 left | 1 right
+flag = 1 # 0 left | 1 right, It help us to know where the enemy went
+
+inicio = 0
 
 MAX_SPEED = 40
 NORMAL_SPEED = 35
 
+#Will search for the opponent rotating, and after finding it, 
+#it will go straight
 def searchEngine(distance_right, distance_left):
+    global flag, MAX_SPEED, NORMAL_SPEED
     if distance_right < 300 and distance_left == 300:
         # Opponent to the right => Turn right
-        leftSpeed  =  20;
-        rightSpeed = -20;
-    elif distance_right==300 and distance_left<300:
+        left_speed  =  40
+        right_speed =  35
+        flag = 1
+    elif distance_right == 300 and distance_left < 300:
         # Opponent to the left => Turn left
-        leftSpeed  = -20;
-        rightSpeed =  20;
-    elif distance_right<300 and distance_left<300:
-        # Opponent in front => Don't turn
-        leftSpeed  = 0;
-        rightSpeed = 0;
-    elif distance_right==300 and distance_left==300:
-        # Opponent lost => Search to the right
-        leftSpeed  =  20;
-        rightSpeed = -20;
+        left_speed  =  40
+        right_speed =  35
+        flag = 0
+    elif distance_right < 300 and distance_left < 300:
+        # Opponent in front => Go
+        left_speed  = 40
+        right_speed = 40
+    else:# Lost the opponent
+        if flag == 0: #Last time seen was to the left
+            left_speed  = 0
+            right_speed = 35
+        else: #Last time seen was to the right
+            left_speed  = 35
+            right_speed = 0
+    return left_speed, right_speed
 
+# Will see if the sensor of the robot is above the white line (borders)
+# Will return True or False
 def not_in_danger(front_right, front_left):
-    return (front_right == 0 or front_left == 0)
+    return (front_right == 0 and front_left == 0)
 
-
+# Main function that will loop 60 times in a second
 def control(front_right, front_left, back_right, back_left, distance_right, distance_left):
-    global flag, MAX_SPEED, NORMAL_SPEED
-    left_speed  = 0
-    right_speed = 0
+    global inicio, flag, MAX_SPEED, NORMAL_SPEED
     
+    ## This block sets the initials conditions and retreat (back sensors) strategy in case needed
+    if(back_left > 0):
+            right_speed = MAX_SPEED*0.5
+            left_speed = MAX_SPEED*0.8
+            inicio = 1
+            
+    elif(back_right > 0):
+            left_speed = MAX_SPEED*0.5
+            right_speed = MAX_SPEED*0.8
+            inicio = 1
+            
+    elif(inicio == 0):
+            right_speed = -MAX_SPEED*0.8
+            left_speed = -MAX_SPEED*0.6
+    ## WIP - Work In Progress
+    
+    # Everything is fine, so it will search for the enemy and attack it
     if (not_in_danger(front_right, front_left)):
-        if ( (distance_right < 300) or (distance_left < 300) ):
-            left_speed = MAX_SPEED
-            right_speed = MAX_SPEED
-        elif ( (distance_right < 300) ):
-            left_speed = MAX_SPEED
-            right_speed = NORMAL_SPEED
-            flag = 1
-        elif ( (distance_left < 300) ):
-            left_speed = NORMAL_SPEED
-            right_speed = MAX_SPEED
-            flag = 0
-        else:
-            searchEngine(distance_right, distance_left)
-            left_speed = NORMAL_SPEED if flag == 0 else 0
-            right_speed = NORMAL_SPEED if flag == 1 else 0
+        left_speed, right_speed = searchEngine(distance_right, distance_left)
     else:
-        left_speed = -MAX_SPEED
-        right_speed = -MAX_SPEED
+    # The robot is in danger, so it got to go back with all speed (reverse)    
+
+        left_speed, right_speed  = -MAX_SPEED, -MAX_SPEED
         
     return {
         'leftSpeed': left_speed,
