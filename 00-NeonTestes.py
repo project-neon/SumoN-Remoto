caa0e7856808fd3874d247fb15ea4f590c1b4bc4
Matt <matheus.canassa@hotmail.com>
2021-06-07 17:55:23 -0300

    fix/feat: initial function working, added timer count

--- 00-NeonTestes.py
+++ 00-NeonTestes.py
@@ -1,5 +1,6 @@
 flag = 1 # 0 left | 1 right, It help us to know where the enemy went
 
+counter = 0
 inicio = 0
 
 MAX_SPEED = 40
@@ -7,8 +8,11 @@ NORMAL_SPEED = 35
 
 #Will search for the opponent rotating, and after finding it, 
 #it will go straight
+
+
 def searchEngine(distance_right, distance_left):
     global flag, MAX_SPEED, NORMAL_SPEED
+    
     if distance_right < 300 and distance_left == 300:
         # Opponent to the right => Turn right
         left_speed  =  40
@@ -39,26 +43,30 @@ def not_in_danger(front_right, front_left):
 
 # Main function that will loop 60 times in a second
 def control(front_right, front_left, back_right, back_left, distance_right, distance_left):
-    global inicio, flag, MAX_SPEED, NORMAL_SPEED
     
-    ## This block sets the initials conditions and retreat (back sensors) strategy in case needed
-    if(back_left > 0):
+    global counter, inicio, flag, MAX_SPEED, NORMAL_SPEED
+        
+    if counter >= 0: #Time counter function, might come in handy
+        counter += 1
+    
+    #Initial retreat conditions block for maneuvering
+    if back_left > 0: 
             right_speed = MAX_SPEED*0.5
             left_speed = MAX_SPEED*0.8
             inicio = 1
             
-    elif(back_right > 0):
+    elif back_right > 0:
             left_speed = MAX_SPEED*0.5
             right_speed = MAX_SPEED*0.8
             inicio = 1
             
-    elif(inicio == 0):
-            right_speed = -MAX_SPEED*0.8
-            left_speed = -MAX_SPEED*0.6
-    ## WIP - Work In Progress
+    elif inicio == 0:
+            right_speed = -MAX_SPEED
+            left_speed = -MAX_SPEED
+    # Current maneuver: retreat and reposition            
     
     # Everything is fine, so it will search for the enemy and attack it
-    if (not_in_danger(front_right, front_left)):
+    elif (not_in_danger(front_right, front_left)):
         left_speed, right_speed = searchEngine(distance_right, distance_left)
     else:
     # The robot is in danger, so it got to go back with all speed (reverse)    
@@ -68,5 +76,11 @@ def control(front_right, front_left, back_right, back_left, distance_right, dist
     return {
         'leftSpeed': left_speed,
         'rightSpeed': right_speed,
-        'log': [  ]
+        'log': [ 
+        
+            { 'name': 'Timer',  'value':   counter,    'min': 0, 'max': 600 }
+          # { 'name': 'Front Right', 'value':   front_right,   'min': 0, 'max': 1 }
+          # { 'name': 'Distance Left', 'value': distance_left, 'min': 0, 'max': 300 }
+        
+        ]
     }
