643c5510a8d7c097be058d01de2b90672345761a
FOGUETE <lucasm_leite@hotmail.com>
2021-06-07 21:45:13 -0300

    Version4: unifiquei os 'ifs' dos sensores traseiros e criei um 'if' para verificar se o inimigo está no meio dos sensores de distância

--- RAKETE.js
+++ RAKETE.js
@@ -1,35 +1,36 @@
-//V3
+//V4
 var inicio = 0;
 var left_speed, right_speed;
 var speed = 45;
 function control(front_right, front_left, back_right, back_left, distance_right, distance_left) {
 
-   if(back_left > 0){
-        right_speed = speed*0.4;
-        left_speed = speed*0.8;
+    if (back_left > 0 || back_right > 0){
+        left_speed = speed;
+        right_speed = speed;
         inicio = 1;
-    }else if(back_right > 0){
-        left_speed = speed*-0.7;
-        right_speed = speed*0.7;
-        inicio = 1;
-    }else if(inicio === 0 ){
-        left_speed = speed * -0.9;
-        right_speed = speed * -0.9;
+    }else if(inicio == 0){
+        right_speed = -speed*0.9;
+        left_speed = -speed*0.6;
     }else if (front_right > 0){
-        left_speed = speed*-0.8;
-        right_speed = speed*-0.7;
+        left_speed = speed*0.9;
+        right_speed = speed*-0.9;
+        inicio = 1;
     }else if(front_left > 0){
-        right_speed = speed*-0.8;
-        left_speed = speed*-0.7;
+        right_speed = speed*0.4;
+        left_speed = speed*-0.9;
+        inicio = 1;
+    }else if((distance_left < 300 && distance_right < 300) && ((distance_left-distance_right) < 10)){
+        right_speed = speed*0.9;
+        left_speed = speed*0.9;
     }else if(distance_left < 300){
-        right_speed = speed*0.9;
-        left_speed = speed*0.9;
+        right_speed = speed*0.8;
+        left_speed = speed*0.8;
     }else if(distance_right < 300){
-        left_speed = speed*0.9;
-        right_speed = speed*0.9;
+        left_speed = speed*0.8;
+        right_speed = speed*0.8;
     }else if((Math.abs(distance_right - distance_left))<=20){
-        left_speed = speed*0.9;
-        right_speed = speed*0;
+        left_speed = speed*0.6;
+        right_speed = speed*-0.5;
     }
     return {
         leftSpeed: left_speed,
