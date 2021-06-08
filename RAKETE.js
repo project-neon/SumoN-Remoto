5f07154ed145ea2081de4049add78d49642d6ca3
FOGUETE <lucasm_leite@hotmail.com>
2021-06-07 21:58:42 -0300

    Version9: tentei acrescentar ao controle proporcional de velocidade uma ñova função para memorizar a ultima posição do inimigo

--- RAKETE.js
+++ RAKETE.js
@@ -1,40 +1,62 @@
-//V8
-var inicio = 0;
-var left_speed, right_speed;
+//V9
+var speedL, speedR;
 var speed = 45;
+var mem = 'r'; // m -> mid     r -> right     l -> left
+var inicio = 0;
+
 function control(front_right, front_left, back_right, back_left, distance_right, distance_left) {
+    var sensorR = distance_right;
+    var sensorL = distance_left;
     
-    var error = 0 - (distance_left - distance_right);
+    var error = 0 - (sensorL - sensorR);
     
-        
-    if (front_right > 0.8 || front_left > 0.8){
-        left_speed = speed*0.9;
-        right_speed = speed*-0.9;
-        
-    }else if(Math.abs(error) <= 10 && distance_left < 300){
-        right_speed = speed;
-        left_speed = speed;
-        
-    }else if(error>0){
-        right_speed = 0.9*speed*(error/300);
-        left_speed = 0.9*speed*(error/300);
-        
-    }else if(error<0){
-        error = Math.abs(error);
-        right_speed = 0.9*speed*(error/300);
-        left_speed = 0.9*speed*(error/300);
-        
-    }else if(distance_left < 300 || distance_right < 300){
-        right_speed = speed*0.8;
-        left_speed = speed*0.8;
-        
-    }else if((Math.abs(error))<=20){
-        left_speed = speed*0.7;
-        right_speed = speed*-0.1;
+    speedR = speed * (error/300);
+    speedL = speed * (error/300) * (-1);
+    
+    if(Math.abs(error) <= 10 && (sensorR < 300 && sensorL < 300)){
+        mem = 'm';
+    }else if (error > 0){
+        mem = 'l';
+    }else if (error < 0){
+        mem = 'r';
+    }else{
+        speedR = speed * -0.2;
+        speedL = speed * 0.9;
+    }
+    
+    switch (mem){
+            
+        case 'l':
+            speedR = speed * 0.8;
+            speedL = speed * 0.2;
+            break;
+        case 'r':
+            speedL = speed * 0.8;
+            speedR = speed * 0.2;
+            break;
+        case 'm':
+            speedR = speed * 0.9;
+            speedL = speed * 0.9;
+            break;
+        default: break;
+    }
+    
+  
+    
+    
+    if(front_left > 0.8 || front_left > 0.8){
+        speedR = speed*-0.5;
+        speedL = speed*-0.9;
+    }
+    
+    if(inicio < 25){
+        speedR = speed * -0.9;
+        speedL = speed * -0.15;
+        inicio++;
     }
     return {
-        leftSpeed: left_speed,
-        rightSpeed: right_speed,
+        leftSpeed: speedL,
+        rightSpeed: speedR,
         /*log : [
             {name: 'Left',  value: left_speed, min: -45, max: 45},
             {name: 'Right', value: right_speed, min: -45, max: 45},
