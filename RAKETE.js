9d5305db7215fb8e9535b674535927154d563ac3
FOGUETE <lucasm_leite@hotmail.com>
2021-06-07 21:53:59 -0300

    Version7: inclui os 'ifs' dos sensores traseiros novamente, inclui uma função para funcionar como contador e comentei o código em pt-BR

--- RAKETE.js
+++ RAKETE.js
@@ -1,31 +1,51 @@
-//V6
-var inicio = 0;
-var left_speed, right_speed;
-var speed = 45;
-function control(front_right, front_left, back_right, back_left, distance_right, distance_left) {
+//V7
+var left_speed, right_speed;   //variáveis usadas para atribuir valores aos motores das rodas.
+var speed = 45;                //valor máximo da velocidde;                
+var count = 30;                //contador inicial de tempo  (tempo real=count/60)
 
-   if(inicio == 0){
-        right_speed = speed*0.9;
-        left_speed = speed*0.6;
-        inicio = 1;
-    }else if (front_right > 0){
+
+function control(front_right, front_left, back_right, back_left, distance_right, distance_left) {
+    
+    
+    // vai pra trás no inicio por 0,5 segundo
+    // estratégia para sair do campo de visão inicialmente
+    if (count>0 && (distance_right-distance_left)==0){
+        left_speed = speed*-0.8;
+        right_speed = speed*-0.7;
+        count = count - 1;
+    }
+    
+    //ao terminar a contagem, verifica se chegou na borda e começa a procurar o adversário
+    else if(back_left > 0){
+        right_speed = speed*0.5;
         left_speed = speed*0.8;
-        right_speed = speed*-0.9;
-        inicio = 1;
+    }else if(back_right > 0){
+        left_speed = speed*0.5;
+        right_speed = speed*0.8;
+    }else if(front_right > 0){
+        left_speed = speed * -0.8;
+        right_speed = speed * 0.8;
     }else if(front_left > 0){
-        right_speed = speed*-0.9;
-        left_speed = speed*0.7;
-        inicio = 1;
-    }else if(distance_left < 300){
+        left_speed = speed * 0.8;
+        right_speed = speed * -0.8;
+    }
+    
+    //quando encontra algum adversário, gira para o lado em que ele está.
+    else if(distance_left < 300){
         right_speed = speed*0.9;
-        left_speed = speed*0.4;
+        left_speed = speed*0.7;
     }else if(distance_right < 300){
-        left_speed = speed*0.9;
-        right_speed = speed*0.4;
-    }else if((Math.abs(distance_right - distance_left))<=20){
         left_speed = speed*0.9;
         right_speed = speed*0.7;
     }
+    
+    //se não encontra nenhum inimigo, gira em seu próprio eixo procurando o adversário
+    else if((Math.abs(distance_right - distance_left))<=1){
+        left_speed = speed*0.6;
+        right_speed = speed*-0.3;
+    }
+    
+    //outpus
     return {
         leftSpeed: left_speed,
         rightSpeed: right_speed,
