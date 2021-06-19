f92ab33b6eccb925314a63902ceb84c6a07c8da6
FOGUETE <lucasm_leite@hotmail.com>
2021-06-18 23:04:08 -0300

    refactor: change initial condition

--- RAKETE.py
+++ RAKETE.py
@@ -1,88 +1,119 @@
-############ ------------ VERSION 11 ------------ ############
+############ ------------ VERSION 12 ------------ ############
 
 ############ ---------- START VARIABLES --------- ############
-inicio = 'r'        #initial condition, 'r'=rear, 'f'=forward, 'e'=estrategy
-left_speed = 0
-right_speed = 0
-speed = 45
-enemyPos = 1        #enemy position, '0'=left, '1'=center, '2'=right
-last = 1            #last enemy position
+
+inicio = 'r'              ##initial condition, 'r'=rear, 'f'=foward, 'e'=estrategy
+left_speed = 0            ##motor's variables
+right_speed = 0           ##motor's variables
+speed = 41                ##speed MAX set point
+enemyPos = 1              ##memory of enemy position, 0=left, 1=center, 2=right
+last = 1                  ##last enemy position memory
+count = 1                 ##counter
+
 
 ############ --------- START OF THE PROGRAM --------- ############
+
 def control(front_right, front_left, back_right, back_left, distance_right, distance_left):
- 
-    global inicio, left_speed, right_speed, speed, enemyPos, last
+
+    global inicio, left_speed, right_speed, speed, enemyPos, last, count
     
+#########################################    
+#           INITIAL CONDITION           #
+#########################################
+        
+    ##initial condition
+    if inicio == 'r' :
+        ##go to the back
+        left_speed = speed * -1.0
+        right_speed = speed * -1.0
+        inicio = 'e'
+        return {
+        'leftSpeed': left_speed,
+        'rightSpeed': right_speed
+          }
+        
+    if count > 8 :    
+    
+
 ##########################################    
 #   DISCOVER THE POSITION OF THE ENEMY   #
-##########################################
-
-    enemyPos = distance_left - distance_right
+##########################################    
     
-    if enemyPos >= 10:
-        enemyPos = 2        #enemy is in the righ
-        
-    elif enemyPos <= -10:
-        enemyPos = 0        #enemy is in the left
-
-    else:
-        if last == 2:
-            enemyPos = 2    #last enemy position to the right
-        elif last == 0:
-            enemyPos = 0    #last enemy position to the left
-        else:
-            enemyPos = 1    #enemy is in the center or lost enemy
-
-    last = enemyPos         #capture the last enemy position
+        enemyPos = distance_left - distance_right
+    
+   
+        if enemyPos >= 10 :
+            enemyPos = 2 ##enemy to the right
+        elif enemyPos <= -10 :
+            enemyPos = 0 ##enemy to the left
+        else :
+            if last == 2 :
+                enemyPos = 2 ##last enemy position to the right
+            elif last == 0 :
+                enemyPos = 0 ##last enemy position to the left
+            else :
+                enemyPos = 1 ##enemy in the center or lost enemy
+            
+        last = enemyPos    ##capture the last enemy position
     
 ##########################################    
 #             VERIFICATIONS              #
-##########################################
-
-    #checks if the robot has reached the edge by the 'back_sensor'
-    if back_left > 0.5:
-        right_speed = speed*0.7
-        left_speed = speed*0.9
-        inicio = 'f';    
-        
-    #checks if the robot has reached the edge by the 'front_sensor'
-    elif front_right > 0.5:
-        right_speed = speed*0.875
-        left_speed = speed*-0.875
-        inicio = 'e'
-        last = 1
-    elif front_left > 0.5:
-        right_speed = speed*-0.875
-        left_speed = speed*0.875
-        inicio = 'e'
-        last = 1
+########################################## 
+    
+    ##if the back_sensor > 0.5, go to the 'foward'
+        if back_left > 0.5 :
+            left_speed = speed*0.8
+            right_speed = speed*0.7875
+            inicio = 'f'
+    
+    ##if the front_sensor > 0.0, go to 'estrategy'    
+        elif front_right > 0 :
+            ##turn to the left
+            left_speed = speed*-0.984
+            right_speed = speed*0.984
+            inicio = 'e'
+            last = 1 ##'last' changed
+        elif front_left > 0 :
+            ##turn to the right
+            right_speed = speed*-0.984
+            left_speed = speed*0.984
+            inicio = 'e'
+            last = 1 ##'last' changed
 
 ##########################################    
 #             MAKE DECISIONS             #
+##########################################       
+    
+    ##if the enemy position is '2', then the last position is 'right'     
+        elif enemyPos == 2 :
+            ##go! go! go!
+            right_speed = speed*0.9
+            left_speed = speed*1.0
+        
+    ##if the enemy position is '0', then the last position is 'left'    
+        elif enemyPos == 0 :
+            ##go! go! go! again kkkkkkk
+            left_speed = speed*0.9
+            right_speed = speed*1.0
+
+##########################################    
+#            ESTRATEGY, MAYBE            #
 ##########################################
-    
-    #rotates to the side where the enemy is
-    elif enemyPos == 2:
-        right_speed = speed*0.9
-        left_speed = speed*0.9
+
         
-    elif enemyPos == 0:
-        right_speed = speed*0.85
-        left_speed = speed*0.9    
+    ##if the enemy is ahead
+        elif enemyPos == 1 and distance_left < 300 :
+            ##go! go! go! more faster!!!!!
+            left_speed = speed*1.0
+            right_speed = speed*1.0
         
-#########################################   
-#           INITIAL CONDITION           #
-#########################################
+    ##if 'inicio' = 'f', o to the foward    
+        elif inicio == 'f' :
+            ##turn looking for enemy
+            left_speed = speed*1.0
+            right_speed = speed*-1.0
     
-    #goes back
-    elif inicio == 'r':
-        left_speed = speed * -0.9
-        right_speed = speed * -0.9
-    
-    #goes forward, turns looking for the enemy
-    elif inicio == 'f':
-        left_speed = speed * 0.9
-        right_speed = speed * -0.9
+    count+=1
     
 ###########################################   
 #                 OUTPUTS                 #
@@ -91,4 +122,4 @@ def control(front_right, front_left, back_right, back_left, distance_right, dist
     return {
         'leftSpeed': left_speed,
         'rightSpeed': right_speed
-    }
+          }
