6e8dcd401c7c6fbe12d429b50191c2ce1f5f1539
FOGUETE <lucasm_leite@hotmail.com>
2021-06-18 23:01:26 -0300

    refactor: code change for version 1

--- RAKETE.py
+++ RAKETE.py
@@ -1,70 +1,52 @@
-############ ------------ VERSION 09 ------------ ############
+############ ------------ VERSION 10 ------------ ############
 
 ############ --------- START VARIABLES --------- ############
-error = 0
-sensorR = 0
-sensorL = 0
 inicio = 0
-speedL = 0
-speedR = 0
+left_speed = 0
+right_speed = 0
 speed = 45
-mem = 'r'        #variable used to 'mem'orize the position of the enemy 
-                 # ---> 'm'=mid // 'r'=right // 'l'=left
 
 ############ --------- START OF THE PROGRAM --------- ############
 def control(front_right, front_left, back_right, back_left, distance_right, distance_left):
  
-    global error, sensorL, sensorR, inicio, speedL, speedR, speed, mem
+    global inicio, left_speed, right_speed, speed
     
-    sensorR = distance_right
-    sensorL = distance_left
+    #checks if the robot has reached the edge by the 'back_sensor'
+    if back_left > 0 :
+        right_speed = speed*0.6
+        left_speed = speed*0.8
+        inicio = 1
+    elif back_right > 0:
+        left_speed = speed*0.6
+        right_speed = speed*0.8
+        inicio = 1
     
-    error = 0 - (distance_left-distance_right)
+    #initial condition, the robot goes back
+    elif inicio == 0:
+        right_speed = -speed*0.8
+        left_speed = -speed*0.6
+        
+    #checks if the robot has reached the edge by the 'front_sensor'
+    elif front_right > 0:
+        left_speed = speed*-0.8
+        right_speed = speed*-0.7
+    elif front_left > 0:
+        right_speed = speed*-0.8
+        left_speed = speed*-0.7
     
-    speedR = speed * (error/300)
-    speedL = speed * (error/300) * (-1)
-        
-    #checks the position of the enemy    
-    if abs(error) <= 10 and (sensorR < 300 and sensorL < 300):
-        mem='m'
-    
-    elif error > 0:
-        mem='l'
-        
-    elif error < 0:
-        mem='r'
-        
-    #search for the enemy    
-    else:
-        speedR = speed * -0.2
-        speedL = speed * 0.9
-        
     #rotates to the side where the enemy is
-    if mem == 'l':
-        speedR = speed * 0.8
-        speedL = speed * 0.2
-    
-    elif mem == 'r':
-        speedR = speed * 0.2
-        speedL = speed * 0.8
-    
-    else: #mem = 'm'
-        speedR = speed * 0.9
-        speedL = speed * 0.9
-        
-    #checks if the robot has reached the edge by the 'front_sensor'    
-    if front_left > 0.8 or front_right > 0.8:
-        speedR = speed*-0.5
-        speedL = speed*-0.9
-        
-    #goes back for 0.42 seconds, it happens only once
-    if inicio < 25:
-        speedR = speed * -0.9
-        speedL = speed * -0.15
-        inicio += 1
+    elif distance_left < 300:
+        right_speed = speed*0.9
+        left_speed = speed*0.9
+    elif distance_right < 300:
+        left_speed = speed*0.9
+        right_speed = speed*0.9
+    elif((-1)*(distance_right - distance_left))<=20:
+        left_speed = speed*0.8
+        right_speed = speed*-0.0    
     
     #outputs
     return {
-        'leftSpeed': speedL,
-        'rightSpeed': speedR
+        'leftSpeed': left_speed,
+        'rightSpeed': right_speed
     }
