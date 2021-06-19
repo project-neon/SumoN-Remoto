dd32a5c763e0199f3d79f6a5f7b17d3d86d7a5a0
FOGUETE <lucasm_leite@hotmail.com>
2021-06-18 22:56:53 -0300

    refactor: change in the initial behavior of the robot

--- RAKETE.py
+++ RAKETE.py
@@ -1,4 +1,4 @@
-############ ------------ VERSION 05 ------------ ############
+############ ------------ VERSION 06 ------------ ############
 
 ############ --------- START VARIABLES --------- ############
 inicio = 0
@@ -8,38 +8,37 @@ speed = 45
 
 ############ --------- START OF THE PROGRAM --------- ############
 def control(front_right, front_left, back_right, back_left, distance_right, distance_left):
-    
+ 
     global inicio, left_speed, right_speed, speed
     
-    #checks if the robot has reached the edge by the 'back_sensor'
-    if back_left > 0 or back_right > 0:
-        right_speed = speed*1.0
-        left_speed = speed*1.0
+    #initial condition, the robot goes forward   
+    if inicio == 0 and (distance_left == 300 and distance_right == 300):
+        right_speed = speed*0.9
+        left_speed = speed*0.6
         inicio = 1
-        
-    #initial condition, the robot goes back    
-    elif inicio == 0 and (distance_left == 300 and distance_right == 300):
-        right_speed = speed*-0.9
-        left_speed = speed*-0.6
         
     #checks if the robot has reached the edge by the 'front_sensor'    
-    elif front_right > 0 or front_left > 0:
-        left_speed = speed*0.9
+    elif front_right > 0:
+        left_speed = speed*0.8
+        right_speed = speed*-0.9
+        inicio = 1
+    elif front_left > 0:
+        left_speed = speed*0.7
         right_speed = speed*-0.9
         inicio = 1
         
-    #Go forward when you spot the enemy
-    elif (distance_left < 300 and distance_right < 300) and (distance_left-distance_right) < 10:
+    #rotates to the side where the enemy is
+    elif distance_left < 300:
         right_speed = speed*0.9
+        left_speed = speed*0.4
+    elif distance_right < 300:
+        right_speed = speed*0.4
         left_speed = speed*0.9
-    elif distance_left < 300 or distance_right < 300:
-        right_speed = speed*0.8
-        left_speed = speed*0.8
         
     #search for the enemy    
     elif(abs(distance_right - distance_left))<=20:
-        left_speed = speed*0.6
-        right_speed = speed*-0.5
+        left_speed = speed*0.9
+        right_speed = speed*0.7
     
     #outputs
     return {
